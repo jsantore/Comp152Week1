46b9a839ae3213b9b8e77f3616864df0491c40c6
johns <jsantore@bridgew.edu>
2025-05-28 12:43:43 -0400

    read in many names

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 void main(){
-    var commandLineReader = new Scanner(System.in);
-    System.out.print("How old are you?:");
-    var age = commandLineReader.nextInt();
-    var dogYears = age*7;
-    System.out.println("You are "+ age +
-            " years old, so you are "+
-            dogYears +" in dog years.");
+    var classList = new ArrayList<String>();
+    var reader = new Scanner(System.in);
+    while(true){
+        System.out.println("Enter student name");
+        var studentName = reader.nextLine();
+        if(studentName.equals("")){
+            break;
+        }
+        classList.add(studentName);
+    }
+    for(var name : classList) {
+        System.out.println(name);
+    }
 }
