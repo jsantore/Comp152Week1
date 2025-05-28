04dc2810902aa3cc1ee9e7abf80e252e9b7de356
johns <jsantore@bridgew.edu>
2025-05-28 11:56:42 -0400

    Hello world with an input read

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-void main(){
+import java.util.Scanner;
 
+
+void main(){
+    var inputReader = new Scanner(System.in);
+    System.out.println("Please enter your name:");
+    var name = inputReader.nextLine();
+    System.out.println("Hello " + name + "!");
 }
 //hello
