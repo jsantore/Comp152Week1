817cb635b6c3d5939508d35e1503926781a19bd4
johns <jsantore@bridgew.edu>
2025-05-28 12:17:02 -0400

    input with numbers now

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 
 void main(){
-    var inputReader = new Scanner(System.in);
-    System.out.println("Please enter your name:");
-    var name = inputReader.nextLine();
-    System.out.println("Hello " + name + "!");
+    var commandLineReader = new Scanner(System.in);
+    System.out.print("How old are you?:");
+    var age = commandLineReader.nextInt();
+    var dogYears = age*7;
+    System.out.println("You are "+ age +
+            " years old, so you are "+
+            dogYears +" in dog years.");
 }
-//hello
