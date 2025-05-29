14cc38a8c981e54a5f378e7ee847ba5da3513d3c
johns <jsantore@bridgew.edu>
2025-05-29 10:47:30 -0400

    random guess program

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Random;
+import java.util.Scanner;
 
 
-void main() throws IOException {
-
-    var allStudents = Files.readAllLines(Paths.get("ClassList.txt"));
-    //print out the list of students
-    for (var student : allStudents) {
-        var line = student.split(",");
-        System.out.println(line[0]);
+void main()  {
+    var scanner = new Scanner(System.in);
+    var random = new Random();
+    var number = random.nextInt(10)+1;
+    while (true) {
+        System.out.println("Guess a number between 1 and 10");
+        int guess = scanner.nextInt();
+        if (guess == number) {
+            System.out.println("You got it!");
+            break;
+        } else {
+            System.out.println("You didn't get it!");
+        }
     }
 }
 
