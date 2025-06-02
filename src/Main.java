b260bb030c6fce4231827fea0be054cc20005d2b
johns <jsantore@bridgew.edu>
2025-06-02 09:39:10 -0400

    End of Thursday class

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,44 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.awt.Point;
 
 
-void main()  {
-    var scanner = new Scanner(System.in);
-    var random = new Random();
-    var number = random.nextInt(10)+1;
-    while (true) {
-        System.out.println("Guess a number between 1 and 10");
-        int guess = scanner.nextInt();
-        if (guess == number) {
-            System.out.println("You got it!");
-            break;
-        } else {
-            System.out.println("You didn't get it!");
-        }
-    }
+//void main()  {
+//  var first = new Point(1, 1);
+//  var second = new Point(4, 5);
+//  var distance = calcDistance(first, second);
+//  System.out.println(distance);
+//  var value1 = 30;
+//  var value2 = 4.5f;
+//  paramDemo(value1, value2);
+//  System.out.println("Value1 "+value1);
+//
+//}
+
+public static int calcDistance(Point p1, Point p2){
+    var deltaX = p2.x - p1.x;
+    var deltaY = p2.y - p1.y;
+    return (int) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 }
 
-//Felipe here
+public static void paramDemo(int number1, float number2)
+{
+   System.out.println("number1: " + number1);
+   number1 = 10;
+   System.out.println("number1: " + number1);
+}
+
+public static void returnDemo(){
+    System.out.println("blewfliugjkhgsdff ");
+}
+
+static double Balance(double oldBalance, double interestRate){
+    var newBalance = oldBalance *interestRate+oldBalance;
+    return newBalance;
+}
 
 
-//jeremias here!
+void main(){
+    var newBalance = Balance(10, 0.2);
+    System.out.println("After a year your Balance is:"  + newBalance );
+}
